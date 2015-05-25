cd26db244a4d349498e5321fa061a20b45f66acb
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-25 13:03:30 -0700

    setup and got the gallery implemented

---
+++ www/js/app.js
@@ -0,0 +1,29 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var ionicApp = angular.module('starter', ['ionic']);
+
+ionicApp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+ionicApp.controller('ExampleController', function($scope){
+  $scope.images = [];
+
+  $scope.loadImages = function(){
+    for(var i = 0; i < 100; i++){
+      $scope.images.push({id: i, src: 'http://placehold.it/50x50'})
+    }
+  }
+});
